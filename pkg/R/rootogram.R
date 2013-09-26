2f2cbc5d1b2b6efbec5ff1776a90c8b2eb11eef7
kleiber (Christian Kleiber) <christian.kleiber@unibas.ch>
2013-09-26 16:08:10 +0000

    now with rootogram.gamlss and rootogram.gam. Warning: not all distributions permitted with gamlss. gam version very primitive. Problems expected.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/countreg@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rootogram.R
+++ pkg/R/rootogram.R
@@ -380,3 +380,184 @@ autoplot.rootogram <- function(object,
   ## return with annotation
   rval
 }
+
+rootogram.gamlss <- function(object, newdata = NULL, breaks = NULL,
+  max = NULL, xlab = NULL, main = NULL, width = NULL, ...) 
+{
+  family <- substr(family(object)[2], 1L, 30L)
+  if(!(family %in% c("Negative Binomial type I", "Negative Binomial type II", "Poisson", 
+                     "Poisson.Inverse.Gaussian", "Sichel", "Yule", "Delaporte"))) {
+    stop("family currently not supported")
+  }
+  
+  ## observed response
+  mt <- terms(object)
+  mf <- if(is.null(newdata)) {
+    model.frame(object)
+  } else {
+    model.frame(mt, newdata, na.action = na.omit)
+  }
+  y <- model.response(mf)
+  mu <- predict(object, what = "mu", newdata = newdata, type = "response", na.action = na.omit)
+  sigma <- predict(object, what = "sigma", newdata = newdata, type = "response", na.action = na.omit)
+  
+  if(family == "gaussian") {
+    ## estimated standard deviation (ML)
+    s <- sqrt(mean(residuals(object)^2))
+
+    ## breaks
+    if(is.null(breaks)) breaks <- "Sturges"
+    yhist <- hist(y, plot = FALSE, breaks = breaks)
+    breaks <- yhist$breaks
+    obsrvd <- yhist$count
+
+    ## expected frequencies
+    p <- matrix(NA, nrow = length(y), ncol = length(breaks) - 1L)
+    for(i in 1L:ncol(p)) p[, i] <- pnorm(yhist$breaks[i + 1L], mean = mu, sd = s) -
+      pnorm(yhist$breaks[i], mean = mu, sd = s)
+    expctd <- colSums(p)
+  } else if(family == "binomial") {
+    ## successes and failures
+    if(NCOL(y) < 2L) y <- cbind(y, 1L - y)
+
+    ## number of attempts
+    size <- unique(rowSums(y))
+    if(length(size) > 1L) stop("rootogram only applicable to binomial distributions with same size")
+    at <- 0L:size
+    breaks <- -1L:size + 0.5
+    
+    ## observed and expected
+    obsrvd <- table(factor(y[, 1L], levels = at))
+    p <- matrix(NA, length(mu), length(at))
+    for(i in at) p[, i + 1L] <- dbinom(i, prob = mu, size = size)
+    expctd <- colSums(p)
+  } else {
+    ## observed frequencies
+    max0 <- if(is.null(max)) max(1.5 * max(y), 20L) else max  
+    obsrvd <- table(factor(y, levels = 0L:max0))
+
+    ## expected frequencies
+    at <- 0L:max0
+    p <- matrix(NA, length(mu), length(at))
+    if(family == "Poisson") {
+      for(i in at) p[, i + 1L] <- dpois(i, lambda = mu)
+    } 
+    if(family == "Poisson.Inverse.Gaussian") {
+      for(i in at) p[, i + 1L] <- dPIG(i, mu = mu, sigma = sigma)
+    } 
+    if(family == "Negative Binomial type II") {
+      for(i in at) p[, i + 1L] <- dNBII(i, mu = mu, sigma = sigma)
+    } 
+    if(family == "Negative Binomial type I") {
+      for(i in at) p[, i + 1L] <- dNBI(i, mu = mu, sigma = sigma)
+    } 
+#else {
+#      theta <- object$sigma.coefficients
+#      if(is.null(theta)) theta <- get("(Intercept)", object$sigma.coefficients)
+#      for(i in at) p[, i + 1L] <- dnbinom(i, mu = mu, size = theta)
+#    }
+    expctd <- colSums(p)
+
+    ## try to guess a good maximum
+    if(is.null(max)) {
+      max <- if(all(expctd >= 1L)) max0 else max(ceiling(mean(y)), min(which(expctd < 1L)) - 1L)
+      max <- min(max, length(expctd) - 1L)
+    }
+    breaks <- -1L:max + 0.5
+
+    ## observed and expected frequencies
+    obsrvd <- obsrvd[1L:(max + 1L)]
+    expctd <- expctd[1L:(max + 1L)]
+  }
+
+  if(is.null(xlab)) xlab <- as.character(attr(mt, "variables"))[2L]
+  if(is.null(main)) main <- deparse(substitute(object))
+  rootogram.default(obsrvd, expctd, breaks = breaks,
+    xlab = xlab, main = main,
+    width = if(family == "gaussian") 1 else 0.9, ...)  
+}
+
+rootogram.gam <- function(object, newdata = NULL, breaks = NULL,
+                          max = NULL, xlab = NULL, main = NULL, width = NULL, ...) 
+{
+  family <- substr(family(object)$family, 1L, 17L)
+  if(!(family %in% c("Negative Binomial", "poisson", "binomial", "gaussian"))) {
+    stop("family currently not supported")
+  }
+  
+  ## observed response
+  mt <- terms(object)
+  mf <- if(is.null(newdata)) {
+    model.frame(object)
+  } else {
+    model.frame(mt, newdata, na.action = na.omit)
+  }
+  y <- model.response(mf)
+  mu <- predict(object, newdata = object$model, type = "response", na.action = na.omit)
+  
+  if(family == "gaussian") {
+    ## estimated standard deviation (ML)
+    s <- sqrt(mean(residuals(object)^2))
+    
+    ## breaks
+    if(is.null(breaks)) breaks <- "Sturges"
+    yhist <- hist(y, plot = FALSE, breaks = breaks)
+    breaks <- yhist$breaks
+    obsrvd <- yhist$count
+    
+    ## expected frequencies
+    p <- matrix(NA, nrow = length(y), ncol = length(breaks) - 1L)
+    for(i in 1L:ncol(p)) p[, i] <- pnorm(yhist$breaks[i + 1L], mean = mu, sd = s) -
+      pnorm(yhist$breaks[i], mean = mu, sd = s)
+    expctd <- colSums(p)
+  } else if(family == "binomial") {
+    ## successes and failures
+    if(NCOL(y) < 2L) y <- cbind(y, 1L - y)
+    
+    ## number of attempts
+    size <- unique(rowSums(y))
+    if(length(size) > 1L) stop("rootogram only applicable to binomial distributions with same size")
+    at <- 0L:size
+    breaks <- -1L:size + 0.5
+    
+    ## observed and expected
+    obsrvd <- table(factor(y[, 1L], levels = at))
+    p <- matrix(NA, length(mu), length(at))
+    for(i in at) p[, i + 1L] <- dbinom(i, prob = mu, size = size)
+    expctd <- colSums(p)
+  } else {
+    ## observed frequencies
+    max0 <- if(is.null(max)) max(1.5 * max(y), 20L) else max  
+    obsrvd <- table(factor(y, levels = 0L:max0))
+    
+    ## expected frequencies
+    at <- 0L:max0
+    p <- matrix(NA, length(mu), length(at))
+    if(family == "poisson") {
+      for(i in at) p[, i + 1L] <- dpois(i, lambda = mu)
+    } else {
+      theta <- object$theta
+      if(is.null(theta)) theta <- get(".Theta", environment(family(object)$variance))
+      for(i in at) p[, i + 1L] <- dnbinom(i, mu = mu, size = theta)
+    }
+    expctd <- colSums(p)
+    
+    ## try to guess a good maximum
+    if(is.null(max)) {
+      max <- if(all(expctd >= 1L)) max0 else max(ceiling(mean(y)), min(which(expctd < 1L)) - 1L)
+      max <- min(max, length(expctd) - 1L)
+    }
+    breaks <- -1L:max + 0.5
+    
+    ## observed and expected frequencies
+    obsrvd <- obsrvd[1L:(max + 1L)]
+    expctd <- expctd[1L:(max + 1L)]
+  }
+  
+  if(is.null(xlab)) xlab <- as.character(attr(mt, "variables"))[2L]
+  if(is.null(main)) main <- deparse(substitute(object))
+  rootogram.default(obsrvd, expctd, breaks = breaks,
+                    xlab = xlab, main = main,
+                    width = if(family == "gaussian") 1 else 0.9, ...)  
+}
+
