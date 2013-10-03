212a9ca681331c8f1c7b26b5392dd18c3594a04d
kleiber (Christian Kleiber) <christian.kleiber@unibas.ch>
2013-10-03 16:32:55 +0000

    rootogram.gamlss now with weights. somewhat messy
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/countreg@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rootogram.R
+++ pkg/R/rootogram.R
@@ -382,7 +382,7 @@ autoplot.rootogram <- function(object,
 }
 
 rootogram.gamlss <- function(object, newdata = NULL, breaks = NULL,
-  max = NULL, xlab = NULL, main = NULL, width = NULL, ...) 
+                             max = NULL, xlab = NULL, main = NULL, width = NULL, ...) 
 {
   family <- substr(family(object)[2], 1L, 30L)
   if(!(family %in% c("Negative Binomial type I", "Negative Binomial type II", "Poisson", 
@@ -398,28 +398,30 @@ rootogram.gamlss <- function(object, newdata = NULL, breaks = NULL,
     model.frame(mt, newdata, na.action = na.omit)
   }
   y <- model.response(mf)
+  w <- object$weights
+  if(is.null(w)) w <- rep(1, NROW(y))
   mu <- predict(object, what = "mu", newdata = newdata, type = "response", na.action = na.omit)
   sigma <- predict(object, what = "sigma", newdata = newdata, type = "response", na.action = na.omit)
+  nu <- predict(object, what = "nu", newdata = newdata, type = "response", na.action = na.omit)
   
   if(family == "gaussian") {
     ## estimated standard deviation (ML)
-    s <- sqrt(mean(residuals(object)^2))
-
+    s <- sqrt(weighted.mean(residuals(object)^2, w))
+    
     ## breaks
     if(is.null(breaks)) breaks <- "Sturges"
-    yhist <- hist(y, plot = FALSE, breaks = breaks)
-    breaks <- yhist$breaks
-    obsrvd <- yhist$count
-
+    breaks <- hist(y[w > 0], plot = FALSE, breaks = breaks)$breaks
+    obsrvd <- as.vector(xtabs(w ~ cut(y, breaks, include.lowest = TRUE)))
+    
     ## expected frequencies
     p <- matrix(NA, nrow = length(y), ncol = length(breaks) - 1L)
-    for(i in 1L:ncol(p)) p[, i] <- pnorm(yhist$breaks[i + 1L], mean = mu, sd = s) -
-      pnorm(yhist$breaks[i], mean = mu, sd = s)
-    expctd <- colSums(p)
+    for(i in 1L:ncol(p)) p[, i] <- pnorm(breaks[i + 1L], mean = mu, sd = s) -
+      pnorm(breaks[i], mean = mu, sd = s)
+    expctd <- colSums(p * w)
   } else if(family == "binomial") {
     ## successes and failures
     if(NCOL(y) < 2L) y <- cbind(y, 1L - y)
-
+    
     ## number of attempts
     size <- unique(rowSums(y))
     if(length(size) > 1L) stop("rootogram only applicable to binomial distributions with same size")
@@ -427,15 +429,15 @@ rootogram.gamlss <- function(object, newdata = NULL, breaks = NULL,
     breaks <- -1L:size + 0.5
     
     ## observed and expected
-    obsrvd <- table(factor(y[, 1L], levels = at))
+    obsrvd <- as.vector(xtabs(w ~ factor(y[, 1L], levels = at)))
     p <- matrix(NA, length(mu), length(at))
     for(i in at) p[, i + 1L] <- dbinom(i, prob = mu, size = size)
-    expctd <- colSums(p)
+    expctd <- colSums(p * w)
   } else {
     ## observed frequencies
-    max0 <- if(is.null(max)) max(1.5 * max(y), 20L) else max  
-    obsrvd <- table(factor(y, levels = 0L:max0))
-
+    max0 <- if(is.null(max)) max(1.5 * max(y[w > 0]), 20L) else max  
+    obsrvd <- as.vector(xtabs(w ~ factor(y, levels = 0L:max0)))
+    
     ## expected frequencies
     at <- 0L:max0
     p <- matrix(NA, length(mu), length(at))
@@ -451,30 +453,34 @@ rootogram.gamlss <- function(object, newdata = NULL, breaks = NULL,
     if(family == "Negative Binomial type I") {
       for(i in at) p[, i + 1L] <- dNBI(i, mu = mu, sigma = sigma)
     } 
-#else {
-#      theta <- object$sigma.coefficients
-#      if(is.null(theta)) theta <- get("(Intercept)", object$sigma.coefficients)
-#      for(i in at) p[, i + 1L] <- dnbinom(i, mu = mu, size = theta)
-#    }
-    expctd <- colSums(p)
-
+    if(family == "YULE") {
+      for(i in at) p[, i + 1L] <- dYULE(i, mu = mu)
+    } 
+    if(family == "SICHEL") {
+      for(i in at) p[, i + 1L] <- dSICHEL(i, mu = mu, sigma = sigma, nu = nu)
+    } 
+    if(family == "DEL") {
+      for(i in at) p[, i + 1L] <- dDEL(i, mu = mu, sigma = sigma, nu = nu)
+    } 
+    expctd <- colSums(p * w)
+    
     ## try to guess a good maximum
     if(is.null(max)) {
       max <- if(all(expctd >= 1L)) max0 else max(ceiling(mean(y)), min(which(expctd < 1L)) - 1L)
       max <- min(max, length(expctd) - 1L)
     }
     breaks <- -1L:max + 0.5
-
+    
     ## observed and expected frequencies
     obsrvd <- obsrvd[1L:(max + 1L)]
     expctd <- expctd[1L:(max + 1L)]
   }
-
+  
   if(is.null(xlab)) xlab <- as.character(attr(mt, "variables"))[2L]
   if(is.null(main)) main <- deparse(substitute(object))
   rootogram.default(obsrvd, expctd, breaks = breaks,
-    xlab = xlab, main = main,
-    width = if(family == "gaussian") 1 else 0.9, ...)  
+                    xlab = xlab, main = main,
+                    width = if(family == "gaussian") 1 else 0.9, ...)  
 }
 
 rootogram.gam <- function(object, newdata = NULL, breaks = NULL,
