4996e890e407066844825c5bc971c9600d6bd9b8
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2013-12-16 00:46:45 +0000

    FLXMRnegbin adapted to latest version of flexmix (2.3-11)
    Instead of optimizing the likelihood using profile likelihood, now
    BFGS with analytical gradients is employed.
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/countreg@37 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/FLXMRnegbin.R
+++ pkg/R/FLXMRnegbin.R
@@ -4,7 +4,7 @@ FLXMRnegbin <- function(formula = . ~ ., theta = NULL, offset = NULL)
   .theta <- theta
   
   nbrefit <- function(x, y, w) {
-    fit <- c(glm.fit(x, y, weights = w, offset = offset, family = negative.binomial(theta)),
+    fit <- c(glm.fit(x, y, weights = w, offset = offset, family = MASS::negative.binomial(theta)),
 	     list(call = sys.call(), offset = offset,
 		  control = eval(formals(glm.fit)$control),
 		  method = "weighted.glm.fit"))
@@ -69,63 +69,56 @@ FLXMRnegbin <- function(formula = . ~ ., theta = NULL, offset = NULL)
   }
       
   z@fit <- function(x, y, w, component){
-    theta <- if(is.null(component@parameters$theta)) {
-      if(is.null(.theta)) 1 else .theta
+    if(is.null(component$theta)) {
+      df <- ncol(x)
+      theta <- if(is.null(.theta)) 1 else .theta
+      cf <- glm.fit(x, y, weights = w, family = MASS::negative.binomial(theta),
+        offset = offset, start = component$coef)$coefficients
     } else {
-      nll <- function(th) with(
-        glm.fit(x, y, weights = w, family = negative.binomial(th), offset = offset, start = component@parameters$coef),
-	-sum(w * dnbinom(y, mu = fitted.values, size = th, log = TRUE)))
-      theta <- optimize(nll, c(1/500, 500))$minimum ## FIXME: improve speed
+      ## degrees of freedom
+      df <- ncol(x) + 1
+      
+      ## offset
+      if(is.null(offset)) offset <- 0
+      
+      ## objective function
+      nll <- function(par) {
+        beta <- par[-df]
+        theta <- exp(par[df])
+	mu <- exp(drop(x %*% beta + offset))
+	suppressWarnings(-sum(w * dnbinom(y, mu = mu, size = theta, log = TRUE)))
+      }
+
+      ## corresponding gradients
+      gr <- function(par) {
+        beta <- par[-df]
+        theta <- exp(par[df])
+        mu <- exp(drop(x %*% beta + offset))
+	gr <- drop(y - mu * (y + theta)/(mu + theta))
+	colSums(-w * cbind(gr * x, theta * (digamma(y + theta) - digamma(theta) +
+	  log(theta) + 1 - log(mu + theta) - (y + theta)/(mu + theta))))
+      }
+
+      ## starting values from previous iteration
+      start <- c(component$coef, component$theta)
+      ## if not available: use geometric GLM
+      if(length(start) < df) start <- c(
+        glm.fit(x, y, weights = w, family = MASS::negative.binomial(1), offset = offset)$coefficients,
+	0
+      )
+
+      ## BFGS optimization
+      opt <- optim(par = start, fn = nll, gr = gr, method = "BFGS",
+        control = list(reltol = .Machine$double.eps^(1/1.5), maxit = 500))
+      
+      ## estimated parameters
+      cf <- opt$par[-df]
+      theta <- exp(opt$par[df])
     }
-    fit <- glm.fit(x, y, weights = w, family = negative.binomial(theta), offset = offset, start = component@parameters$coef)
-    with(list(coef = coef(fit), theta = theta, df = ncol(x) + is.null(.theta)),
-	 eval(z@defineComponent))
+    
+    with(list(coef = cf, theta = theta, df = ncol(x) + is.null(.theta)),
+      eval(z@defineComponent))
   }
 
   z
-}
-
-
-if(FALSE) {
-set.seed(1)
-d <- data.frame(x = runif(500, -1, 1))
-d$cluster <- rep(1:2, each = 250)
-d$y <- rnbinom(500, mu = exp(c(1, -1)[d$cluster] + c(0, 3)[d$cluster] * d$x), size = 1)
-
-library("flexmix")
-fm1 <- flexmix(y ~ x, data = d, k = 2, model = FLXMRnegbin(theta = 1))
-fm0 <- flexmix(y ~ x, data = d, k = 2, model = FLXMRnegbin())
-
-parameters(fm1)
-parameters(fm0)
-
-plot(fm1)
-plot(fm0)
-
-table(clusters(fm0), clusters(fm1))
-
-plot(posterior(fm0)[,1] ~ factor(cluster), data = d)
-plot(posterior(fm1)[,1] ~ factor(cluster), data = d)
-plot(posterior(fm0)[,1] ~ posterior(fm1)[,1])
-
-rf1 <- lapply(1:2, function(i) glm(y ~ x, data = d, family = negative.binomial(1), weights = posterior(fm1)[,i]))
-rootogram(rf1[[1]])
-rootogram(rf1[[2]])
-
-rf0 <- lapply(1:2, function(i) glm.nb(y ~ x, data = d, weights = posterior(fm0)[,i]))
-rootogram(rf0[[1]])
-rootogram(rf0[[2]])
-
-"+.rootogram" <- function(e1, e2) {
-  xlab <- paste(unique(c(attr(e1, "xlab"), attr(e2, "xlab"))), collapse = " / ")
-  main <- paste(unique(c(attr(e1, "main"), attr(e2, "main"))), collapse = " / ")
-  e1 <- as.data.frame(e1)
-  e2 <- as.data.frame(e2)
-  e <- e1[e1$x %in% e2$x, ] + e2[e2$x %in% e1$x, ]
-  rootogram.default(structure(e$observed, .Names = e$x/2), e$expected,
-    main = main, xlab = xlab)
-}
-
-rootogram(rf1[[1]]) + rootogram(rf1[[2]])
-rootogram(rf0[[1]]) + rootogram(rf0[[2]])
 }
