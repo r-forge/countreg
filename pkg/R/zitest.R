0526fc189d4bd1e59a91c0540b093105284a9817
kleiber (Christian Kleiber) <christian.kleiber@unibas.ch>
2014-08-29 13:50:37 +0000

    new function zitest(), currently quite primitive
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/countreg@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/zitest.R
@@ -0,0 +1,23 @@
+zitest <- function(object){
+ stopifnot(object$family$family == "poisson")
+
+ n <- nobs(object)
+ y <- if (is.null(object$y)) 
+       model.response(model.frame(object))
+      else object$y
+
+ pzero <- exp(-fitted(object))
+ s1    <- ( (y == 0) - pzero )/pzero
+ s2    <- ( 1 - pzero )/pzero
+ stat  <- sum(s1)^2 / ( sum(s2) - n * mean(y))
+
+ rval <- list(statistic = c(S = stat), 
+              p.value = pchisq(stat, df = 1, lower.tail = FALSE)/2, 
+              alternative = NULL, 
+              method = "Zero-inflation test", 
+              data.name = deparse(substitute(object))
+              )
+ 
+ class(rval) <- "htest"
+ return(rval)
+}
