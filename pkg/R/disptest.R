6c7c9d8d8a90eca109dc007dfb0d19f9256e0f03
kleiber (Christian Kleiber) <christian.kleiber@unibas.ch>
2014-09-03 17:57:10 +0000

    new type = lrtNB2, classical LRT. Need to think about treatment of alternatives.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/countreg@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/disptest.R
+++ pkg/R/disptest.R
@@ -1,11 +1,12 @@
 disptest <- function(object, 
-  type = c("scoreNB2", "scoreNB2adj", "scoreNB1", "scoreNB1adj", "scoreKatz"), 
+  type = c("lrtNB2", "scoreNB2", "scoreNB2adj", "scoreNB1", "scoreNB1adj", "scoreKatz"), 
   trafo = NULL, alternative = c("greater", "two.sided", "less"))
 {
 ## sanity checks  
   if(!inherits(object, "glm") || family(object)$family != "poisson")
     stop("only Poisson GLMs can be tested")  ## maybe later GLMs more generally?
   alternative <- match.arg(alternative)
+  if(type != "scoreKatz") alternative <- "greater"  ## warning: quick fix
   type <- match.arg(type)
 
 ## preprocessing
@@ -14,32 +15,45 @@ disptest <- function(object,
   n <- nobs(object)
 
 ## test statistics
-  if(type == "scoreNB2") 
+  if(type == "lrtNB2") {
+  m_nb <- glm.nb(object$formula, data = object$data)
+  STAT <- 2 * (logLik(m_nb) - logLik(object))
+  pval <- pchisq(STAT, df = 1, lower.tail = FALSE)/2 
+  }
+   
+  if(type == "scoreNB2") {
   STAT <- sum((y - yhat)^2 - y) / sqrt(2 * sum(yhat^2))
-  
+  pval <- pnorm(STAT, lower.tail = FALSE)
+  }
+    
   if(type == "scoreNB2adj") {
   h <- hatvalues(object)
   STAT <- sum((y - yhat)^2 - y  + h * yhat) / sqrt(2 * sum(yhat^2)) 
+  pval <- pnorm(STAT, lower.tail = FALSE)  
   }
 
-  if(type == "scoreNB1") 
+  if(type == "scoreNB1") {
   STAT <- sum(( (y - yhat)^2 - y) / yhat ) / sqrt(2 * n)
+  pval <- pnorm(STAT, lower.tail = FALSE)
+  }  
 
   if(type == "scoreNB1adj") {
   h <- hatvalues(object)
   STAT <- sum(( (y - yhat)^2 - y + h * yhat) / yhat ) / sqrt(2 * n)
+  pval <- pnorm(STAT, lower.tail = FALSE)
   }
 
-  if(type == "scoreKatz") 
+  if(type == "scoreKatz") { 
   STAT <- sum(( (y - 1) * y  - yhat^2) / yhat ) / sqrt(2 * n)
-
+  pval <- switch(alternative, "greater"   = pnorm(STAT, lower.tail = FALSE),
+		                      "two.sided" = pnorm(abs(STAT), lower.tail = FALSE) * 2,
+		                      "less"      = pnorm(STAT) )
+  }
+  
 ## collect output  
   rval <- list(
            statistic = c(z = STAT),
-           p.value = switch(alternative,
-                      "greater"   = pnorm(STAT, lower.tail = FALSE),
-		              "two.sided" = pnorm(abs(STAT), lower.tail = FALSE) * 2,
-		              "less"      = pnorm(STAT) ),
+           p.value = pval,
 #	       estimate = EST,
 #	       null.value = NVAL,
 	       alternative = alternative,
@@ -54,9 +68,6 @@ disptest <- function(object,
 }
 
 ## TODO:
-## 1. compare with DCluster::DeanB() and DCluster::DeanB2()
-## and unify
-##
 ## 2. also fix DCluster::test.nb.pois() and pscl::odTest()
 ## proposed interface:
 ##   poistest(object, object2 = NULL)
