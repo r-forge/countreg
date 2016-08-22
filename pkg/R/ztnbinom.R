57a2bda2356b7f4a9a3b206dab7eebe8d60ebf1b
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2016-08-22 17:10:50 +0000

    improve theta/size canonicalization
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/countreg@110 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ztnbinom.R
+++ pkg/R/ztnbinom.R
@@ -1,25 +1,15 @@
 ## ztnbinom: Zero-truncated negative binomial
 dztnbinom <- function(x, mu, theta, size, log = FALSE) {
-  if(!missing(size)) {
-    if(missing(theta)) {
-      theta <- size
-    } else {
-      stop("only one of 'theta' and 'size' must be specified")
-    }
-  }
+  if(!missing(theta) & !missing(size)) stop("only 'theta' or 'size' may be specified")
+  if(!missing(size)) theta <- size
   rval <- dnbinom(x, mu = mu, size = theta, log = TRUE) -
     pnbinom(0, mu = mu, size = theta, lower.tail = FALSE, log.p = TRUE)
   if(log) rval else exp(rval)
 }
 
 sztnbinom <- function(x, mu, theta, size, parameter = c("mu", "theta"), drop = TRUE) {
-  if(!missing(size)) {
-    if(missing(theta)) {
-      theta <- size
-    } else {
-      stop("only one of 'theta' and 'size' must be specified")
-    }
-  }
+  if(!missing(theta) & !missing(size)) stop("only 'theta' or 'size' may be specified")
+  if(!missing(size)) theta <- size
   parameter <- sapply(parameter, function(x) match.arg(x, c("mu", "theta")))
   s <- snbinom(x, mu = mu, size = theta, parameter = parameter, drop = FALSE)
   logratio <- pnbinom(0, mu = mu, size = theta, log.p = TRUE) -
