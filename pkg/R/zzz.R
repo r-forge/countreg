09d1271f4b40ae0522a67dbe7be91a1380c96c43
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2023-12-09 03:41:09 +0000

    https
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/countreg@186 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -3,7 +3,7 @@
     packageStartupMessage(
       "Starting from 'countreg' 0.3-0 the package was refactored:\n",
       "* Rootograms and other visualizations are in 'topmodels' on R-Forge.\n",
-      "  http://topmodels.R-Forge.R-project.org/articles/topmodels.html\n",
+      "  https://topmodels.R-Forge.R-project.org/articles/topmodels.html\n",
       "* Distribution functions (d/p/q/r) are in 'distributions3' on CRAN.\n",
       "  https://www.zeileis.org/news/user2022/")
   }
